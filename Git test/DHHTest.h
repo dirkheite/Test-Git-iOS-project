bef624489fc3257a87a981ba52f85089cf483c40
Arianna Heite <macbook@Macbooks-MacBook-Air.local>
2014-09-01 13:23:34 -0700

    added a test class

---
+++ Git test/DHHTest.h
@@ -0,0 +1,13 @@
+//
+//  DHHTest.h
+//  Git test
+//
+//  Created by Arianna Heite on 9/1/14.
+//  Copyright (c) 2014 Arianna Heite. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface DHHTest : NSObject
+
+@end
---
+++ Git test/DHHTest.m
@@ -0,0 +1,13 @@
+//
+//  DHHTest.m
+//  Git test
+//
+//  Created by Arianna Heite on 9/1/14.
+//  Copyright (c) 2014 Arianna Heite. All rights reserved.
+//
+
+#import "DHHTest.h"
+
+@implementation DHHTest
+
+@end
